c54ee74ec3d560dda8d8de024d736b19f6508635
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-06-08 23:33:59 +0200

    chore: added cards typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./App.module.scss";
+import card from "./card.png";
 
 function App() {
-  return <div className={styles.App}></div>;
+  return (
+    <div className={styles.App}>
+      <img src={card} />
+    </div>
+  );
 }
 
 export default App;
