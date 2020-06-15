484c7e84fb39c6e1ef4928fe29dfb6da4886ef11
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-06-16 00:03:45 +0200

    chore: styled top bar title

--- src/UI/TopBar/TopBar.tsx
+++ src/UI/TopBar/TopBar.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import ToolBar from "../ToolBar/ToolBar";
 import appIco from "../../static/appIco.png";
 import styles from "./TopBar.module.scss";
 
 const TopBar: React.FC = (props) => {
   return (
-    <div className={styles.topBar__bar}>
-      <img
-        className={styles["topBar__bar--icon"]}
-        src={appIco}
-        alt="Application Icon"
-      />
-      Solitaire
-    </div>
+    <>
+      <div className={styles.topBar__bar}>
+        <img
+          className={styles["topBar__bar--icon"]}
+          src={appIco}
+          alt="Application Icon"
+        />
+        <span className={styles["topBar__bar--title"]}>Solitaire</span>
+      </div>
+      <ToolBar />
+    </>
   );
 };
 
