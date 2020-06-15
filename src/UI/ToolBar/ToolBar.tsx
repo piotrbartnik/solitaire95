eb6707e162485292d78a08aba71d2571a077c657
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-06-16 00:04:07 +0200

    feat: added first version of toolbar

---
+++ src/UI/ToolBar/ToolBar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ToolBar.module.scss";
+
+const ToolBar: React.FC = (props) => {
+  return <div className={styles.toolBar__bar}></div>;
+};
+
+export default ToolBar;
