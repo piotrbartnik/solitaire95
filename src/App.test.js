698ff6fa6187912560a2658061c1e6dc3835cd2f
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-06-18 22:19:59 +0200

    test: added test for timer

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import { debug } from 'console';
 
 describe('renders App page', () => {
   it('and check if it has the top bar', () => {
