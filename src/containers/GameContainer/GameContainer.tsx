bd7173e3661e44345473ac4d92f1f674097fb62a
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-06-24 21:32:24 +0200

    chore: restructurize and styled container with card

--- src/containers/GameContainer/GameContainer.tsx
+++ src/containers/GameContainer/GameContainer.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { CardDestination } from "../../UI";
+import { Card } from "../../components";
 import styles from "./GameContainer.module.scss";
 
 const GameContainer: React.FC = () => {
   return (
-    <div className={styles.gameContainer}>
-      <div className={styles.gameContainer__cardDestination}>
-        <CardDestination />
-        <CardDestination />
-        <CardDestination />
-        <CardDestination />
+    <div className={styles.gameUIBorder}>
+      <div className={styles.gameContainer}>
+        <div className={styles.gameContainer__cardDestination}>
+          <CardDestination />
+          <CardDestination />
+          <CardDestination />
+          <CardDestination />
+        </div>
+        <div className={styles.gameContainer__cardDestination}>
+          <Card />
+        </div>
       </div>
     </div>
   );
