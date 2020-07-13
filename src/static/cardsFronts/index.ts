f6a608131e8eb4276ccf782559b0421067d24788
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-07-13 22:03:52 +0200

    chore: added all cards fronts

--- src/static/cardsFronts/index.ts
+++ src/static/cardsFronts/index.ts
@@ -11,6 +11,45 @@ import sixOfHearts from "./hearts/sixOfHearts.png";
 import tenOfHearts from "./hearts/tenOfHearts.png";
 import twoOfHearts from "./hearts/twoOfHearts.png";
 import threeOfHearts from "./hearts/threeOfHearts.png";
+import aceOfClubs from "./clubs/aceOfClubs.png";
+import eigthOfClubs from "./clubs/eigthOfClubs.png";
+import fiveOfClubs from "./clubs/fiveOfClubs.png";
+import fourOfClubs from "./clubs/fourOfClubs.png";
+import jackOfClubs from "./clubs/jackOfClubs.png";
+import kingOfClubs from "./clubs/kingOfClubs.png";
+import nineOfClubs from "./clubs/nineOfClubs.png";
+import queenOfClubs from "./clubs/queenOfClubs.png";
+import sevenOfClubs from "./clubs/sevenOfClubs.png";
+import sixOfClubs from "./clubs/sixOfClubs.png";
+import tenOfClubs from "./clubs/tenOfClubs.png";
+import twoOfClubs from "./clubs/twoOfClubs.png";
+import threeOfClubs from "./clubs/threeOfClubs.png";
+import aceOfSpades from "./spades/aceOfSpades.png";
+import eigthOfSpades from "./spades/eigthOfSpades.png";
+import fiveOfSpades from "./spades/fiveOfSpades.png";
+import fourOfSpades from "./spades/fourOfSpades.png";
+import jackOfSpades from "./spades/jackOfSpades.png";
+import kingOfSpades from "./spades/kingOfSpades.png";
+import nineOfSpades from "./spades/nineOfSpades.png";
+import queenOfSpades from "./spades/queenOfSpades.png";
+import sevenOfSpades from "./spades/sevenOfSpades.png";
+import sixOfSpades from "./spades/sixOfSpades.png";
+import tenOfSpades from "./spades/tenOfSpades.png";
+import twoOfSpades from "./spades/twoOfSpades.png";
+import threeOfSpades from "./spades/threeOfSpades.png";
+import aceOfDiamonds from "./diamonds/aceOfDiamonds.png";
+import eigthOfDiamonds from "./diamonds/eigthOfDiamonds.png";
+import fiveOfDiamonds from "./diamonds/fiveOfDiamonds.png";
+import fourOfDiamonds from "./diamonds/fourOfDiamonds.png";
+import jackOfDiamonds from "./diamonds/jackOfDiamonds.png";
+import kingOfDiamonds from "./diamonds/kingOfDiamonds.png";
+import nineOfDiamonds from "./diamonds/nineOfDiamonds.png";
+import queenOfDiamonds from "./diamonds/queenOfDiamonds.png";
+import sevenOfDiamonds from "./diamonds/sevenOfDiamonds.png";
+import sixOfDiamonds from "./diamonds/sixOfDiamonds.png";
+import tenOfDiamonds from "./diamonds/tenOfDiamonds.png";
+import twoOfDiamonds from "./diamonds/twoOfDiamonds.png";
+import threeOfDiamonds from "./diamonds/threeOfDiamonds.png";
 
 export const cardFrontsImages: { [char: string]: string } = {
   aceOfHearts,
@@ -26,4 +65,43 @@ export const cardFrontsImages: { [char: string]: string } = {
   tenOfHearts,
   twoOfHearts,
   threeOfHearts,
+  aceOfClubs,
+  eigthOfClubs,
+  fiveOfClubs,
+  fourOfClubs,
+  jackOfClubs,
+  kingOfClubs,
+  nineOfClubs,
+  queenOfClubs,
+  sevenOfClubs,
+  sixOfClubs,
+  tenOfClubs,
+  twoOfClubs,
+  threeOfClubs,
+  aceOfSpades,
+  eigthOfSpades,
+  fiveOfSpades,
+  fourOfSpades,
+  jackOfSpades,
+  kingOfSpades,
+  nineOfSpades,
+  queenOfSpades,
+  sevenOfSpades,
+  sixOfSpades,
+  tenOfSpades,
+  twoOfSpades,
+  threeOfSpades,
+  aceOfDiamonds,
+  eigthOfDiamonds,
+  fiveOfDiamonds,
+  fourOfDiamonds,
+  jackOfDiamonds,
+  kingOfDiamonds,
+  nineOfDiamonds,
+  queenOfDiamonds,
+  sevenOfDiamonds,
+  sixOfDiamonds,
+  tenOfDiamonds,
+  twoOfDiamonds,
+  threeOfDiamonds,
 };
