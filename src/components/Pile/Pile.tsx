c6df96d8b8889efc95c0e02dd7dba1b65fc661b8
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-08-11 22:23:57 +0200

    chore: add first schema for card piles

--- src/components/Pile/Pile.tsx
+++ src/components/Pile/Pile.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Card } from "..";
+import styles from "./Pile.module.scss";
 
 const Pile: React.FC = () => {
   return (
-    <div>
-      <Card front={"kingOfHearts"} back={"acorns"} isTurnedBack={false} />
+    <div className={styles.pile__container}>
+      <div className={styles.pile__firstCard}>
+        <Card front={"kingOfHearts"} back={"acorns"} isTurnedBack={false} />
+      </div>
+      <div className={styles.pile__secondCard}>
+        <Card front={"kingOfHearts"} back={"acorns"} isTurnedBack={false} />
+      </div>
+      <div className={styles.pile__thirdCard}>
+        <Card front={"kingOfHearts"} back={"acorns"} isTurnedBack={false} />
+      </div>
     </div>
   );
 };
