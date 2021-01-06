4e4c54a317a27afefbe22660c3772da545e678c0
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-01-06 21:24:29 +0100

    chore: resolve image in external module

--- components/my-module/webpack.config.js
+++ components/my-module/webpack.config.js
@@ -11,26 +11,16 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.(png|jpg)$/, //Customise according to your need
+        test: /\.(png|jpg)$/,
         use: [
           {
             loader: "url-loader",
-            options: {
-              limit: 10000,
-            },
           },
         ],
       },
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
