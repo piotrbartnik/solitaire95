d7f4ca1608076e019fa4e89f6d811713c0901b2c
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-01-15 22:15:12 +0100

    chore: clean webpack config in component module

--- components/solitaire95/webpack.config.js
+++ components/solitaire95/webpack.config.js
@@ -1,20 +1,15 @@
 const path = require("path");
 module.exports = {
   watch: true,
-  mode: "none",
   entry: "./src/index.ts",
   externals: [
     {
       react: {
         root: "React",
-        commonjs2: "react",
-        commonjs: "react",
         amd: "react",
       },
       "react-dom": {
         root: "ReactDOM",
-        commonjs2: "react-dom",
-        commonjs: "react-dom",
         amd: "react-dom",
       },
     },
@@ -42,14 +37,10 @@ module.exports = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
-    alias: {
-      // Needed when library is linked via `npm link` to app
-      react: path.resolve("./node_modules/react"),
-    },
   },
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "lib"),
-    libraryTarget: "umd",
+    libraryTarget: "amd",
   },
 };
