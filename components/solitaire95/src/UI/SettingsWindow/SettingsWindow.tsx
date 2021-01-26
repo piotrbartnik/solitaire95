a32126d23e3446a5d3eaea50c778a68447d98cfb
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-01-26 21:41:03 +0100

    feat: add custom drag layer

--- components/solitaire95/src/UI/SettingsWindow/SettingsWindow.tsx
+++ components/solitaire95/src/UI/SettingsWindow/SettingsWindow.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDrop, useDrag, useDragLayer } from "react-dnd";
+import { getEmptyImage } from "react-dnd-html5-backend";
 import { itemTypes } from "../../configs/dragndropConfig";
 import { TopBar, Button, CloseButton } from "../index";
 import styles from "./SettingsWindow.module.scss";
@@ -12,6 +13,62 @@ type propTypes = {
   height?: string;
   buttons?: { text: string; onClick: () => void }[];
   closeButtonAction?: () => void;
+};
+
+const CustomDragLayer = (props: any) => {
+  const { size } = props;
+
+  const { itemType, currentOffset, isDragging } = useDragLayer((monitor) => ({
+    itemType: monitor.getItemType(),
+    currentOffset: monitor.getSourceClientOffset(),
+    isDragging: monitor.isDragging(),
+  }));
+
+  const layerStyles: any = {
+    position: "fixed",
+    pointerEvents: "none",
+    zIndex: 1,
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "transparent",
+    display: !currentOffset ? "none" : "block",
+  };
+
+  function renderItem() {
+    switch (itemType) {
+      case itemTypes.WINDOW:
+        return (
+          <div
+            style={{
+              width: size[0],
+              height: size[1],
+              border: "2px dotted #3f3f3f",
+            }}
+          />
+        );
+      default:
+        return null;
+    }
+  }
+
+  if (!isDragging) {
+    return null;
+  }
+  return (
+    <div style={layerStyles}>
+      <div
+        style={{
+          position: "absolute",
+          top: currentOffset?.y,
+          left: currentOffset?.x,
+        }}
+      >
+        {renderItem()}
+      </div>
+    </div>
+  );
 };
 
 const SettingsWindow: React.FC<propTypes> = (props) => {
@@ -27,17 +84,13 @@ const SettingsWindow: React.FC<propTypes> = (props) => {
 
   const [windowPosition, setWindowPosition] = useState([100, 100]);
 
-  const { currentOffset } = useDragLayer((monitor) => ({
-    initialOffset: monitor.getInitialSourceClientOffset(),
-    currentOffset: monitor.getSourceClientOffset(),
-  }));
-
   const [, drop] = useDrop({
     accept: itemTypes.WINDOW,
-    drop: () => {
+    drop: (item, monitor) => {
+      const delta = monitor.getDifferenceFromInitialOffset();
       setWindowPosition([
-        currentOffset?.y as number,
-        currentOffset?.x as number,
+        windowPosition[0] + (delta?.y as number),
+        windowPosition[1] + (delta?.x as number),
       ]);
     },
     collect: (monitor) => ({
@@ -46,7 +99,7 @@ const SettingsWindow: React.FC<propTypes> = (props) => {
     }),
   });
 
-  const [{ isDragging }, drag] = useDrag({
+  const [, drag, preview] = useDrag({
     item: {
       type: itemTypes.WINDOW,
       windowTitle: windowTitle,
@@ -55,6 +108,10 @@ const SettingsWindow: React.FC<propTypes> = (props) => {
       isDragging: !!monitor.isDragging(),
       item: monitor.getItem(),
     }),
+  });
+
+  useEffect(() => {
+    preview(getEmptyImage(), { captureDraggingState: true });
   });
 
   return (
@@ -68,7 +125,6 @@ const SettingsWindow: React.FC<propTypes> = (props) => {
         style={{
           width: width || "450px",
           height: height || "360px",
-          opacity: (isDragging && 1) || undefined,
           top: `${windowPosition[0]}px`,
           left: `${windowPosition[1]}px`,
         }}
@@ -85,6 +141,10 @@ const SettingsWindow: React.FC<propTypes> = (props) => {
             ))}
           </div>
         </div>
+        <CustomDragLayer
+          initialPosition={windowPosition}
+          size={[width || "450px", height || "360px"]}
+        />
       </div>
     </div>
   );
