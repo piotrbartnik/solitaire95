43a571adcc8192759ebec4679022395976a0165d
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-02-09 22:17:00 +0100

    chore: disable typechecking in custom.d.ts file

--- packages/solitaire95/src/components/smart-components/Card/CardHooks.tsx
+++ packages/solitaire95/src/components/smart-components/Card/CardHooks.tsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
 
-export const useSetCardPosition = (isTurnedBack: boolean) => {
+export const useSetCardPosition = (
+  isTurnedBack: boolean
+): readonly [boolean, boolean, Dispatch<SetStateAction<boolean>>] => {
   const [cardPositionFront, changeCardPosition] = useState<boolean>(
     isTurnedBack
   );
--- packages/solitaire95/src/custom.d.ts
+++ packages/solitaire95/src/custom.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "*.png" {
   const content: any;
   export default content;
