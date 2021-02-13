b1fd5053e71a381aabd1c590a1e5faea2503e140
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-02-13 20:00:17 +0100

    chore: exclude d.ts files in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     "^.+\\.(tsx|js|ts)$": "babel-jest",
   },
+  testRegex: ".test.(tsx?|js)$",
   moduleNameMapper: {
     "\\.(png|woff|woff2|mp3)$": "<rootDir>/__mocks__/fileMock.js",
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
