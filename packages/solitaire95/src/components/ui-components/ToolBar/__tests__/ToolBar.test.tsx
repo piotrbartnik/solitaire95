c2381789bf528f658d9ea0e52d565ddbc48b2552
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-02-22 22:03:40 +0100

    test: add tests for toolbar component

---
+++ packages/solitaire95/src/components/ui-components/ToolBar/__tests__/ToolBar.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ToolBar from "../ToolBar";
+
+describe("render ToolBar", () => {
+  it("and check if it is visible", () => {
+    const { container } = render(<ToolBar>Test</ToolBar>);
+    expect(container.querySelector(".toolBar__bar")).toBeVisible();
+  });
+});
