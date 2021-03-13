31cc225a65aeeaff21bed2c94e3e27630243fc82
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-03-13 17:11:05 +0100

    chore: add about solitaire window

---
+++ packages/solitaire95/src/components/smart-components/AboutSolitaireWindow/AboutSolitaireWindow.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { connect } from "react-redux";
+import { toggleAboutWindow } from "../../../store/actions/";
+import { WindowsState } from "../../../store/reducers/";
+import { SettingsWindow } from "../../ui-components";
+// import styles from "./DeckSelect.module.scss";
+
+export type AboutSolitareStateTypes = {
+  isWindowVisible?: boolean;
+};
+export type AboutSolitareDispatchTypes = {
+  toggleAboutWindow: (windowState: boolean) => void;
+};
+
+const AboutSolitareInternal: React.FC<
+  AboutSolitareDispatchTypes & AboutSolitareStateTypes
+> = (props) => {
+  const { isWindowVisible, toggleAboutWindow } = props;
+
+  const okOnClick = () => {
+    toggleAboutWindow(false);
+  };
+
+  return (
+    <SettingsWindow
+      windowTitle={"About Solitare"}
+      buttons={[{ text: "OK", onClick: okOnClick }]}
+      visible={isWindowVisible as boolean}
+      closeButtonAction={() => toggleAboutWindow(false)}
+      width={"528px"}
+    >
+      About Solitaire
+    </SettingsWindow>
+  );
+};
+
+const mapStateToProps = (state: { toggleWindows: WindowsState }) => {
+  return {
+    isWindowVisible: state.toggleWindows.cardBackWindowState,
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    toggleAboutWindow: (payload: boolean) =>
+      dispatch(toggleAboutWindow(payload)),
+  };
+};
+
+export const AboutSolitare = connect<
+  AboutSolitareStateTypes,
+  AboutSolitareDispatchTypes
+>(
+  mapStateToProps,
+  mapDispatchToProps
+)(AboutSolitareInternal);
