bbf12de58fca0d8453854829b4ae7df5c94c12f0
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-05-09 22:22:50 +0200

    chore: change ifs to swtich in undo logic

--- packages/solitaire95/src/Solitaire95.tsx
+++ packages/solitaire95/src/Solitaire95.tsx
@@ -17,64 +17,58 @@ delete persistedState?.toggleWindows;
 const logger: Middleware = (store) => (next) => (action) => {
   const previousState = store.getState();
   let actionToUndo: [string, cardConfigType[], cardConfigType[]] | [] = [];
-  if (action.type === "TAKE_ONE_FROM_STOCK") {
-    actionToUndo = [
-      "TAKE_ONE_FROM_STOCK",
-      previousState.cardDistribution.cardsOnStock,
-      previousState.cardDistribution.cardsFromStock,
-    ];
-    store.dispatch(setUndoAction(actionToUndo));
-  }
-  if (action.type === "REMOVE_CARD_FROM_FOUNDATION") {
-    actionToUndo = [
-      "REMOVE_CARD_FROM_FOUNDATION",
-      previousState.cardsOnFoundation,
-      [],
-    ];
-    store.dispatch(setUndoAction(actionToUndo));
-  }
-  if (action.type === "ADD_CARD_TO_PILE") {
-    const undoState = store.getState().gameState.actionToUndo;
-    if (undoState[0] === "REMOVE_CARD_FROM_FOUNDATION") {
-      undoState[0] = "FROM_FOUNDATION_TO_PILES";
-      undoState[2] = previousState.cardDistribution.cardsOnPiles;
-      store.dispatch(setUndoAction(undoState));
-    } else {
+  const undoState = store.getState().gameState.actionToUndo;
+  switch (action.type) {
+    case "TAKE_ONE_FROM_STOCK":
       actionToUndo = [
-        "ADD_CARD_TO_PILE",
-        previousState.cardDistribution.cardsOnPiles,
-        [],
+        action.type,
+        previousState.cardDistribution.cardsOnStock,
+        previousState.cardDistribution.cardsFromStock,
       ];
       store.dispatch(setUndoAction(actionToUndo));
-    }
-  }
-  if (action.type === "REMOVE_CARD_FROM_STOCK") {
-    const undoState = store.getState().gameState.actionToUndo;
-    if (undoState[0] === "ADD_CARD_TO_FOUNDATION") {
-      undoState[0] = "FROM_STOCK_TO_FOUNDATION";
-    }
-    if (undoState[0] === "ADD_CARD_TO_PILE") {
-      undoState[0] = "FROM_STOCK_TO_PILE";
-    }
-    undoState[2] = previousState.cardDistribution.cardsFromStock;
-    store.dispatch(setUndoAction(undoState));
-  }
-  if (action.type === "REMOVE_CARD_FROM_PILE") {
-    const undoState = store.getState().gameState.actionToUndo;
-    if (undoState[0] === "ADD_CARD_TO_FOUNDATION") {
-      undoState[0] = "FROM_PILE_TO_FOUNDATION";
-    }
-    undoState[2] = previousState.cardDistribution.cardsOnPiles;
-    store.dispatch(setUndoAction(undoState));
+      break;
+    case "REMOVE_CARD_FROM_FOUNDATION":
+      actionToUndo = [action.type, previousState.cardsOnFoundation, []];
+      store.dispatch(setUndoAction(actionToUndo));
+      break;
+    case "ADD_CARD_TO_PILE":
+      if (undoState[0] === "REMOVE_CARD_FROM_FOUNDATION") {
+        undoState[0] = "FROM_FOUNDATION_TO_PILES";
+        undoState[2] = previousState.cardDistribution.cardsOnPiles;
+        store.dispatch(setUndoAction(undoState));
+      } else {
+        actionToUndo = [
+          action.type,
+          previousState.cardDistribution.cardsOnPiles,
+          [],
+        ];
+        store.dispatch(setUndoAction(actionToUndo));
+      }
+      break;
+    case "REMOVE_CARD_FROM_STOCK":
+      if (undoState[0] === "ADD_CARD_TO_FOUNDATION") {
+        undoState[0] = "FROM_STOCK_TO_FOUNDATION";
+      }
+      if (undoState[0] === "ADD_CARD_TO_PILE") {
+        undoState[0] = "FROM_STOCK_TO_PILE";
+      }
+      undoState[2] = previousState.cardDistribution.cardsFromStock;
+      store.dispatch(setUndoAction(undoState));
+      break;
+    case "REMOVE_CARD_FROM_PILE":
+      if (undoState[0] === "ADD_CARD_TO_FOUNDATION") {
+        undoState[0] = "FROM_PILE_TO_FOUNDATION";
+      }
+      undoState[2] = previousState.cardDistribution.cardsOnPiles;
+      store.dispatch(setUndoAction(undoState));
+      break;
   }
   if (action.type.match(/ADD_CARD_TO_[A-Z]+_FOUNDATION/)) {
-    actionToUndo = [
-      "ADD_CARD_TO_FOUNDATION",
-      previousState.cardsOnFoundation,
-      [],
-    ];
+    actionToUndo = [action.type, previousState.cardsOnFoundation, []];
     store.dispatch(setUndoAction(actionToUndo));
   }
+
+  console.log(actionToUndo, undoState);
 
   return next(action);
 };
