5a7aee245fee0a52723416cf5b017e3b58dd327e
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-05-20 21:54:55 +0200

    chore: add solution for pushing all cards to an array to render

--- packages/solitaire95/src/helpers/cardWaterfall.ts
+++ packages/solitaire95/src/helpers/cardWaterfall.ts
@@ -1,48 +1,78 @@
 import { RefObject } from "react";
 
+const whileCallback = (
+  cx: number,
+  cy: number,
+  vy: number,
+  vx: number,
+  decay: number,
+  cardToMove: HTMLElement | null
+) => {
+  let y = 0;
+
+  const cardArray = [];
+
+  while (cx > -126 && cx < window.innerWidth + 126) {
+    cx += vx * 1;
+    cy += vy * 1;
+    vy += decay * 1;
+    const cardClone = cardToMove?.cloneNode(true);
+    y++;
+
+    if (cy >= 918 - 171) {
+      cy = 918 - 171;
+      vy = vy * -1 * 0.7 + (1.0 - Math.random() * 2.0); //(Math.random() *2)
+      if (vy > 0.1) vy = -1;
+    }
+
+    (cardClone as Element)?.setAttribute(
+      "style",
+      `position:fixed; top:${cy}px;
+      left:${cx}px;
+      z-index: ${1000 + y}`
+    );
+    cardArray.push(cardClone);
+  }
+
+  return cardArray;
+};
+
 export const cardWaterfall = (
   mainPageRef: RefObject<HTMLDivElement>,
-  cardsOnfoundationRef: NodeListOf<Element>
+  cardsOnfoundationRef: NodeListOf<Element>,
+  i: number,
+  vy: number | undefined,
+  vx: number | undefined,
+  decay: number | undefined
 ): void => {
-  const allCards = ["king"];
+  console.log(i);
   const pageRef = mainPageRef.current;
-  for (let i = 0; i < allCards.length; i++) {
-    const cardToMove = cardsOnfoundationRef[51 - i]?.parentElement;
-    const parentPosition: number[] = [
-      cardToMove?.getBoundingClientRect().x as number,
-      cardToMove?.getBoundingClientRect().y as number,
-    ];
-    const gameContainer = pageRef?.querySelector("[class*='gameContainer']");
+  const cardToMove = cardsOnfoundationRef[12]?.parentElement;
+  const parentPosition: number[] = [
+    cardToMove?.getBoundingClientRect().x as number,
+    cardToMove?.getBoundingClientRect().y as number,
+  ];
+  const gameContainer = pageRef?.querySelector("[class*='gameContainer']");
 
-    const vx = -4; // next image position from the left
-    let vy = 4; // next image from the top
-    let cx = parentPosition[0]; // position from the left
-    let cy = parentPosition[1]; // position from the top
-    const decay = 0.2;
+  const cx = parentPosition[0]; // position from the left
+  const cy = parentPosition[1]; // position from the top
 
-    let y = 0;
+  const cardsToRender: (Node | undefined)[] = [];
 
-    while (cx > -126 && cx < window.innerWidth + 126) {
-      cx += vx * 1;
-      cy += vy * 1;
-      vy += decay * 1;
-      const cardClone = cardToMove?.cloneNode(true);
-      y++;
+  cardsToRender.push(
+    ...whileCallback(
+      cx,
+      cy,
+      vy as number,
+      vx as number,
+      decay as number,
+      cardToMove
+    )
+  );
 
-      if (cy >= 918 - 171) {
-        cy = 918 - 171;
-        vy = vy * -1 * 0.7 + (1.0 - Math.random() * 2.0); //(Math.random() *2)
-        if (vy > 0.1) vy = -1;
-      }
-      (cardClone as Element)?.setAttribute(
-        "style",
-        `position:fixed; top:${cy}px;
-        left:${cx}px;
-        z-index: ${1000 + y}`
-      );
-      setTimeout(() => {
-        gameContainer?.append(cardClone as Node);
-      }, 10 * y);
-    }
+  for (let y = 0; y < cardsToRender.length; y++) {
+    setTimeout(() => {
+      gameContainer?.append(cardsToRender[y] as Node);
+    }, 10 * y);
   }
 };
