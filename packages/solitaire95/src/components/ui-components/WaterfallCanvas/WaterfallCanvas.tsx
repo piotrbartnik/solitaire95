e83aa8a1b6831dc7077c952538814f132798c7f7
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-06-07 22:39:59 +0200

    chore: add full stock drawing

--- packages/solitaire95/src/components/ui-components/WaterfallCanvas/WaterfallCanvas.tsx
+++ packages/solitaire95/src/components/ui-components/WaterfallCanvas/WaterfallCanvas.tsx
@@ -10,14 +10,19 @@ export const WaterfallCanvas: React.FC<WaterfallCanvasPropTypes> = (props) => {
   const { canvasWidth, canvasHeight } = props;
   const canvasRef = useRef(null);
 
-  const drawStocRectangle = (context: CanvasRenderingContext2D) => {
+  const drawStockRectangle = (context: CanvasRenderingContext2D) => {
     context.lineWidth = 2;
     context.beginPath();
     context.moveTo(143, 187);
-    context.arcTo(19, 187, 0, 0, 7);
-    context.arcTo(19, 12, 149, 0, 7);
-    context.arcTo(149, 12, 149, 187, 7);
+    context.arcTo(20, 187, 0, 0, 7);
+    context.arcTo(20, 13, 149, 0, 7);
+    context.arcTo(149, 13, 149, 187, 7);
     context.arcTo(149, 187, 0, 187, 7);
+    context.stroke();
+    context.beginPath();
+    context.lineWidth = 14;
+    context.strokeStyle = "#00ff00";
+    context.arc(84, 99, 53, 0, 2 * Math.PI);
     context.stroke();
   };
 
@@ -26,13 +31,12 @@ export const WaterfallCanvas: React.FC<WaterfallCanvasPropTypes> = (props) => {
 
     if (canvas) {
       const context = (canvas as HTMLCanvasElement).getContext("2d");
-      //Our first draw
       (context as CanvasRenderingContext2D).fillStyle = "#20ac55";
       // context.fillRect(0, 0, context.canvas.width, context.canvas.height);
       // const image = new Image(60, 45);
       // image.src = king;
       // context.drawImage(image, 40, 50);
-      drawStocRectangle(context as CanvasRenderingContext2D);
+      drawStockRectangle(context as CanvasRenderingContext2D);
     }
   }, []);
   return (
