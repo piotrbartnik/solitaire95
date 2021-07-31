0c22a44c3a7ecab42688e8ff86d633140820dac2
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-07-31 11:12:22 +0200

    chore: adjust height of game container for max pile height

--- applications/electron-react-app/public/electron.js
+++ applications/electron-react-app/public/electron.js
@@ -6,7 +6,7 @@ const isDev = require("electron-is-dev");
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 1024, height: 768 });
+  mainWindow = new BrowserWindow({ width: 1024, height: 890 });
   Menu.setApplicationMenu(null);
   mainWindow.loadURL(
     isDev
