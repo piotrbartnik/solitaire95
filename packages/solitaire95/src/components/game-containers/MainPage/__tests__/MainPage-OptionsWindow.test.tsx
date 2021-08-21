b3719abfe87d5dfddaef1abc6033f358f1dcce9a
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-08-21 19:48:53 +0200

    test: add first test for testing options window

---
+++ packages/solitaire95/src/components/game-containers/MainPage/__tests__/MainPage-OptionsWindow.test.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+import { fireEvent, screen } from "@testing-library/react";
+import { dndWrapper, reduxRtlWrapper } from "../../../../helpers/testHelpers";
+import { MainPage } from "../MainPage";
+
+const openOptionsWindow = () => {
+  fireEvent.click(screen.getByRole("button", { name: "Game" }));
+  fireEvent.click(screen.getByRole("button", { name: "Options" }));
+};
+
+describe("render MainPage for Options window testing", () => {
+  it("when dropdown Game -> Options clicked About Options is visible", () => {
+    reduxRtlWrapper(dndWrapper(<MainPage />));
+    openOptionsWindow();
+
+    expect(screen.getByText("Options")).toBeVisible();
+  });
+});
