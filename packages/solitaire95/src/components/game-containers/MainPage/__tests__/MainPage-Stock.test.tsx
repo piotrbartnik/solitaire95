8c30c542eee3916f85cbac1bcae242a10c31644d
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-10-30 21:25:12 +0200

    chore: polish stock tests

--- packages/solitaire95/src/components/game-containers/MainPage/__tests__/MainPage-Stock.test.tsx
+++ packages/solitaire95/src/components/game-containers/MainPage/__tests__/MainPage-Stock.test.tsx
@@ -1,36 +1,46 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import { fireEvent, screen } from "@testing-library/react";
 import { createCards } from "../../../../configs/cardTypes";
 import { dndWrapper, reduxRtlWrapper } from "../../../../helpers/testHelpers";
 import { MainPage } from "../MainPage";
 
-// jest.useFakeTimers();
 const clickUndo = () => {
   fireEvent.click(screen.getByRole("button", { name: "Game" }));
   fireEvent.click(screen.getByRole("button", { name: "Undo" }));
 };
 
+const clubsCards = createCards.filter((card) => card[1] === "clubs").reverse();
+
+const initialState = {
+  cardDistribution: {
+    cardsOnStock: clubsCards,
+    cardsFromStock: [],
+    cardsOnPiles: {},
+    threeCardsOnTable: [],
+  },
+  gameState: {
+    drawType: "drawThree",
+    timerVisible: true,
+    bottomBarVisible: true,
+  },
+};
+
+const initialStateWitcAces = {
+  ...initialState,
+  cardDistribution: {
+    cardsOnStock: [
+      ["ace", "spades", null, "red", "0"],
+      ["ace", "hearts", null, "red", "0"],
+      ["ace", "hearts", null, "red", "0"],
+    ],
+    cardsFromStock: [],
+    cardsOnPiles: {},
+    threeCardsOnTable: [],
+  },
+};
+
 describe("render MainPage with custom state for cards on stock", () => {
-  let clubsCards;
-  let initialState: any;
-  beforeEach(() => {
-    clubsCards = createCards.filter((card) => card[1] === "clubs").reverse();
-    initialState = {
-      cardDistribution: {
-        cardsOnStock: clubsCards,
-        cardsFromStock: [],
-        cardsOnPiles: {},
-        threeCardsOnTable: [],
-      },
-      gameState: {
-        drawType: "drawThree",
-        timerVisible: true,
-        bottomBarVisible: true,
-      },
-    };
-  });
-  it("and for drawThree and click three cards are added to stock", () => {
+  it("and for drawThree and click on stock three cards are added to the table", () => {
     const { container } = reduxRtlWrapper(
       dndWrapper(<MainPage />),
       initialState
@@ -41,20 +51,11 @@ describe("render MainPage with custom state for cards on stock", () => {
     );
     expect(container.querySelectorAll(".cardFront")).toHaveLength(3);
   });
-  it("and for drawThree and double click on ace it is added to foundation", () => {
-    const { container } = reduxRtlWrapper(dndWrapper(<MainPage />), {
-      ...initialState,
-      cardDistribution: {
-        cardsOnStock: [
-          ["ace", "spades", null, "red", "0"],
-          ["ace", "hearts", null, "red", "0"],
-          ["ace", "hearts", null, "red", "0"],
-        ],
-        cardsFromStock: [],
-        cardsOnPiles: {},
-        threeCardsOnTable: [],
-      },
-    });
+  it("and double click on ace, it is added to foundation", () => {
+    const { container } = reduxRtlWrapper(
+      dndWrapper(<MainPage />),
+      initialStateWitcAces
+    );
 
     fireEvent.click(
       container.querySelector(".cardStock__cardHolder") as Element
@@ -67,7 +68,7 @@ describe("render MainPage with custom state for cards on stock", () => {
       container.querySelector(".foundation")?.querySelectorAll(".card")
     ).toHaveLength(1);
   });
-  it("thre cards can be undone", () => {
+  it("thre cards move on stock can be undone", () => {
     const { container } = reduxRtlWrapper(
       dndWrapper(<MainPage />),
       initialState
@@ -85,19 +86,10 @@ describe("render MainPage with custom state for cards on stock", () => {
     expect(container.querySelectorAll(".cardBack")).toHaveLength(13);
   });
   it("and when ace added to foundation it can be undo back to stock and score is substracted", () => {
-    const { container } = reduxRtlWrapper(dndWrapper(<MainPage />), {
-      ...initialState,
-      cardDistribution: {
-        cardsOnStock: [
-          ["ace", "spades", null, "red", "0"],
-          ["ace", "hearts", null, "red", "0"],
-          ["ace", "hearts", null, "red", "0"],
-        ],
-        cardsFromStock: [],
-        cardsOnPiles: {},
-        threeCardsOnTable: [],
-      },
-    });
+    const { container } = reduxRtlWrapper(
+      dndWrapper(<MainPage />),
+      initialStateWitcAces
+    );
 
     fireEvent.click(
       container.querySelector(".cardStock__cardHolder") as Element
