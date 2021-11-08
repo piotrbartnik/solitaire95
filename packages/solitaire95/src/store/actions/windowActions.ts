11d46630d70846fb81d118f3a18597de69747ef0
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-11-08 22:20:25 +0100

    chore: use enum as window actions action types

--- packages/solitaire95/src/store/actions/windowActions.ts
+++ packages/solitaire95/src/store/actions/windowActions.ts
@@ -1,15 +1,19 @@
-import * as actionTypes from "./actionTypes";
+import { ACTION_TYPES } from "./actionTypes";
 
 export type ToggleWindowType = (
   windowState: boolean,
   windowToToggle: string
-) => { type: "TOGGLE_WINDOW"; windowState: boolean; windowToToggle: string };
+) => {
+  type: ACTION_TYPES.TOGGLE_WINDOW;
+  windowState: boolean;
+  windowToToggle: string;
+};
 
 export const toggleWindow: ToggleWindowType = (
   windowState,
   windowToToggle
 ) => ({
-  type: actionTypes.TOGGLE_WINDOW,
+  type: ACTION_TYPES.TOGGLE_WINDOW,
   windowState,
   windowToToggle,
 });
