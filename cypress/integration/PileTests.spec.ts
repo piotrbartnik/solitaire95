fa4d68f9d72e3c4d2317d0fd72a603910157d715
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-11-22 22:28:20 +0100

    chore: extract pile state to separate variable for testing

--- cypress/integration/PileTests.spec.ts
+++ cypress/integration/PileTests.spec.ts
@@ -1,30 +1,43 @@
-// by loading custom commands from the support file,
-// we also load the global Cypress type definition
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../support/index.d.ts" />
 
-describe("Solitaire", () => {
-  beforeEach(function () {
-    cy.clock();
-    cy.viewport(1030, 900);
-  });
-  it("opens in web app", () => {
+const pileTestsInitialState = {
+  cardDistribution: {
+    cardsOnStock: [],
+    cardsFromStock: [],
+    threeCardsOnTable: [],
+    cardsOnPiles: {
+      0: [["ace", "spades", null, "black", "0"]],
+      1: [
+        ["ace", "spades", null, "black", "0"],
+        ["ace", "hearts", null, "red", "0"],
+      ],
+      2: [
+        ["ace", "spades", null, "black", "0"],
+        ["ace", "hearts", null, "red", "0"],
+        ["ace", "diamonds", null, "red", "0"],
+      ],
+      3: [
+        ["ace", "spades", null, "black", "0"],
+        ["ace", "hearts", null, "red", "0"],
+        ["ace", "diamonds", null, "red", "0"],
+        ["ace", "clubs", null, "black", "0"],
+      ],
+      4: [],
+      6: [],
+    },
+  },
+  stockCounter: { stockRevolutions: 0 },
+  timeCounter: { initialTime: 0, scoreTime: 0 },
+};
+
+describe("Solitaire piles", () => {
+  it("aces can be moved to foundations", () => {
     window.localStorage.setItem(
       "solitaireState",
-      JSON.stringify({
-        cardDistribution: {
-          cardsOnStock: [],
-          cardsFromStock: [],
-          threeCardsOnTable: [],
-          cardsOnPiles: {
-            0: [["ace", "spades", null, "black", "0"]],
-          },
-        },
-        stockCounter: { stockRevolutions: 0 },
-        timeCounter: { initialTime: 0, scoreTime: 0 },
-      })
+      JSON.stringify(pileTestsInitialState)
     );
-    cy.visit("http://localhost:3007");
-    cy.get("[data-front=true]").drag("div[class*='foundation']div[id='0']");
+    cy.visit("/");
+    cy.get("[data-cardname=ace]").drag("div[class*='foundation']div[id='0']");
   });
 });
