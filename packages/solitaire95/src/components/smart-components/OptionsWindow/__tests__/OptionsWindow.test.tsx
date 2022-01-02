848e14a3fa3e30c628d00d51167d81bae113cf35
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-01-02 18:41:57 +0100

    test: add checks for rendering options window

--- packages/solitaire95/src/components/smart-components/OptionsWindow/__tests__/OptionsWindow.test.tsx
+++ packages/solitaire95/src/components/smart-components/OptionsWindow/__tests__/OptionsWindow.test.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { reduxRtlWrapper } from "../../../../helpers/testHelpers";
+import { screen } from "@testing-library/react";
+import { reduxRtlWrapper, dndWrapper } from "../../../../helpers/testHelpers";
 import { Options } from "../OptionsWindow";
 
 describe("renders Options Window", () => {
-  it("and check if it has the single div wrapper", () => {
-    const { asFragment } = reduxRtlWrapper(<Options />);
-    expect(asFragment().querySelectorAll("div")).toHaveLength(1);
+  it("and options window with all elements is rendered", () => {
+    reduxRtlWrapper(dndWrapper(<Options />));
+    expect(screen.getByText("Options")).toBeTruthy();
   });
 });
