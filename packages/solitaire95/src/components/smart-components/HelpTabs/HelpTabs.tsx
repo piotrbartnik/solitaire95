c543877b26a1e47688852043aca839179e9f9471
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-01-10 17:42:41 +0100

    chore: add tabs inner elements and dummy nav tab

--- packages/solitaire95/src/components/smart-components/HelpTabs/HelpTabs.tsx
+++ packages/solitaire95/src/components/smart-components/HelpTabs/HelpTabs.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./HelpTabs.module.scss";
 
 type PilePropTypes = {
   children?: JSX.Element;
 };
 
-export const HelpTabs: React.FC<PilePropTypes> = ({
-  children = "Tab here",
-}) => {
+export const HelpTabs: React.FC<PilePropTypes> = ({ children }) => {
+  const [activeTab, setActiveTab] = useState("Contents");
+
   console.log(children);
+
   return (
-    <div className={styles.contentContainer}>
-      <div className={styles.contentContainer__inner}></div>
+    <div className={styles.tabContainer}>
+      <div className={styles.navBar}>
+        <button onClick={() => setActiveTab("Contents")}>Contents</button>
+        <button onClick={() => setActiveTab("Index")}>Index</button>
+        <button onClick={() => setActiveTab("Find")}>Find</button>
+      </div>
+      <div
+        className={[
+          styles.contentContainer,
+          activeTab === "Contents" ? styles.active : undefined,
+        ].join(" ")}
+      >
+        <div className={styles.contentContainer__inner}>Contents</div>
+      </div>
+      <div
+        className={[
+          styles.contentContainer,
+          activeTab === "Index" ? styles.active : undefined,
+        ].join(" ")}
+      >
+        <div className={styles.contentContainer__inner}>Index</div>
+      </div>
+      <div
+        className={[
+          styles.contentContainer,
+          activeTab === "Find" ? styles.active : undefined,
+        ].join(" ")}
+      >
+        <div className={styles.contentContainer__inner}>Find</div>
+      </div>
     </div>
   );
 };
