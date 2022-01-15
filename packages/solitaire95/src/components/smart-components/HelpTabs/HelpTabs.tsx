7ef59a499d8ab41f0fe5fd11df1d449dd60d5fdd
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-01-15 10:10:09 +0100

    chore: extract generic tab group component

--- packages/solitaire95/src/components/smart-components/HelpTabs/HelpTabs.tsx
+++ packages/solitaire95/src/components/smart-components/HelpTabs/HelpTabs.tsx
@@ -1,33 +1,8 @@
-import React, { useState } from "react";
-import { TabNavButton } from "./TabNavButton/TabNavButton";
-import { TabContainer } from "./TabContainer/TabContainer";
-import styles from "./HelpTabs.module.scss";
+import React from "react";
+import { TabGroup } from "./TabGroup/TabGroup";
 
 export const HelpTabs: React.FC = () => {
-  const [activeTab, setActiveTab] = useState("Contents");
-
   const tabs = ["Contents", "Index", "Find"];
 
-  const setActiveTabCallback = (tabToSetActive: string) =>
-    setActiveTab(tabToSetActive);
-
-  return (
-    <div className={styles.tabContainer}>
-      <div className={styles.navBar}>
-        {tabs.map((tabLabel) => (
-          <TabNavButton
-            label={tabLabel}
-            activeTab={activeTab}
-            setActiveTabCallback={setActiveTabCallback}
-            key={tabLabel}
-          />
-        ))}
-      </div>
-      {tabs.map((tabLabel) => (
-        <TabContainer activeTab={activeTab} label={tabLabel} key={tabLabel}>
-          {tabLabel}
-        </TabContainer>
-      ))}
-    </div>
-  );
+  return <TabGroup tabs={tabs} defaultActiveTab="Contents" />;
 };
---
+++ packages/solitaire95/src/components/smart-components/HelpTabs/TabGroup/TabGroup.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { TabNavButton } from "./TabNavButton/TabNavButton";
+import { TabContainer } from "./TabContainer/TabContainer";
+import styles from "./TabGroup.module.scss";
+
+type TabGroupPropTypes = {
+  defaultActiveTab: string;
+  tabs: string[];
+};
+
+export const TabGroup: React.VFC<TabGroupPropTypes> = ({
+  defaultActiveTab,
+  tabs,
+}) => {
+  const [activeTab, setActiveTab] = useState(defaultActiveTab);
+
+  const setActiveTabCallback = (tabToSetActive: string) =>
+    setActiveTab(tabToSetActive);
+
+  return (
+    <div className={styles.tabContainer}>
+      <div className={styles.navBar}>
+        {tabs.map((tabLabel) => (
+          <TabNavButton
+            label={tabLabel}
+            activeTab={activeTab}
+            setActiveTabCallback={setActiveTabCallback}
+            key={tabLabel}
+          />
+        ))}
+      </div>
+      {tabs.map((tabLabel) => (
+        <TabContainer activeTab={activeTab} label={tabLabel} key={tabLabel}>
+          {tabLabel}
+        </TabContainer>
+      ))}
+    </div>
+  );
+};
