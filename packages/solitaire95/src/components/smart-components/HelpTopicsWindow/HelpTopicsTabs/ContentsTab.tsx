2c691b243aeb0b79e7fc5b29447254fef56bcd0f
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-01-18 22:11:14 +0100

    chore: extract text button component

--- packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTabs/ContentsTab.tsx
+++ packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTabs/ContentsTab.tsx
@@ -3,6 +3,39 @@ import { TextSelectField } from "../../../ui-components";
 import styles from "./ContentsTab.module.scss";
 import questionIcon from "./questionHelpIcon.png";
 
+type TextButtonPropTypes = {
+  selectedItem?: number;
+  onClickCallback: () => void;
+  textId: number;
+};
+
+const TextButton: React.FC<TextButtonPropTypes> = ({
+  children,
+  selectedItem,
+  onClickCallback,
+  textId,
+}) => {
+  return (
+    <div className={styles.clickableText__container}>
+      <img
+        className={styles.clickableText__icon}
+        src={questionIcon}
+        alt="question mark icon"
+      />
+      <div
+        className={[
+          selectedItem === textId &&
+            styles["clickableText__textContainer--selected"],
+          styles.clickableText__textContainer,
+        ].join(" ")}
+        onClick={onClickCallback}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
 export const ContentsTab: React.VFC = () => {
   const [selectedItem, setSelectedItem] = useState<number>();
 
@@ -11,40 +44,20 @@ export const ContentsTab: React.VFC = () => {
       Click a topic, and then click Display. Or click another tab, such as
       Index.
       <TextSelectField>
-        <div className={styles.clickableText__container}>
-          <img
-            className={styles.clickableText__icon}
-            src={questionIcon}
-            alt="question mark icon"
-          />
-          <div
-            className={[
-              selectedItem === 0 &&
-                styles["clickableText__textContainer--selected"],
-              styles.clickableText__textContainer,
-            ].join(" ")}
-            onClick={() => setSelectedItem(0)}
-          >
-            How to play Solitaire
-          </div>
-        </div>
-        <div className={styles.clickableText__container}>
-          <img
-            className={styles.clickableText__icon}
-            src={questionIcon}
-            alt="question mark icon"
-          />
-          <div
-            className={[
-              selectedItem === 1 &&
-                styles["clickableText__textContainer--selected"],
-              styles.clickableText__textContainer,
-            ].join(" ")}
-            onClick={() => setSelectedItem(1)}
-          >
-            Scoring information
-          </div>
-        </div>
+        <TextButton
+          onClickCallback={() => setSelectedItem(0)}
+          selectedItem={selectedItem}
+          textId={0}
+        >
+          How to play Solitaire
+        </TextButton>
+        <TextButton
+          onClickCallback={() => setSelectedItem(1)}
+          selectedItem={selectedItem}
+          textId={1}
+        >
+          Scoring information
+        </TextButton>
       </TextSelectField>
     </>
   );
