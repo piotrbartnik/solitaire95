4d0121d29fb8d1cc6abb5facd1a6d00ecdae61cc
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-01-18 21:52:30 +0100

    chore: add contents tab on click action

--- packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTabs/ContentsTab.tsx
+++ packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTabs/ContentsTab.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextSelectField } from "../../../ui-components";
+import styles from "./ContentsTab.module.scss";
 
 export const ContentsTab: React.VFC = () => {
+  const [selectedItem, setSelectedItem] = useState<number>();
+
   return (
     <>
       Click a topic, and then click Display. Or click another tab, such as
       Index.
       <TextSelectField>
-        <p>How to play Solitaire</p>
-        <p>Scoring information</p>
+        <div
+          className={[
+            selectedItem === 0 && styles.selected,
+            styles.textContainer,
+          ].join(" ")}
+          onClick={() => setSelectedItem(0)}
+        >
+          How to play Solitaire
+        </div>
+        <div
+          className={[
+            selectedItem === 1 && styles.selected,
+            styles.textContainer,
+          ].join(" ")}
+          onClick={() => setSelectedItem(1)}
+        >
+          Scoring information
+        </div>
       </TextSelectField>
     </>
   );
