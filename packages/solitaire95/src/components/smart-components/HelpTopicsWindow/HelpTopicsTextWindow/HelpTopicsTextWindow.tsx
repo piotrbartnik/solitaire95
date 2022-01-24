eb101459d4a59a9902625b60e203ed9c4e380dda
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-01-24 21:57:41 +0100

    chore: add styling for help text window

--- packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTextWindow/HelpTopicsTextWindow.tsx
+++ packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTextWindow/HelpTopicsTextWindow.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { SettingsWindow } from "../../../ui-components";
 import questionIcon from "../../../../static/misc/questionHelpIcon.png";
+import styles from "./HelpTopicsTextWindow.module.scss";
+
+const howToPlay = <p>To play Solitaire: starting the game</p>;
 
 export const HelpTopicsTextWindow: React.VFC = () => {
   const [helpTextVisible, setHelpTextVisible] = useState(true);
@@ -9,13 +12,13 @@ export const HelpTopicsTextWindow: React.VFC = () => {
       windowTitle={"Solitaire Help"}
       visible={helpTextVisible}
       closeButtonAction={() => setHelpTextVisible(false)}
-      width={200}
-      height={200}
+      width={400}
+      height={600}
       positionOnWindow={[10, 10]}
       topBarIcon={questionIcon}
       iconHeight="18px"
     >
-      <div>Test</div>
+      <div className={styles.helpTopicsContainer}>{howToPlay}</div>
     </SettingsWindow>
   );
 };
