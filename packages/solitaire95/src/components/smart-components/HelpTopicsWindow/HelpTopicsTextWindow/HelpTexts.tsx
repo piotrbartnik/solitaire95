4be32bdb664069356f06d8c65aff594a1d43a7b6
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-01-30 11:06:27 +0100

    chore: add additional help text window and on mouse over hover action to show it

--- packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTextWindow/HelpTexts.tsx
+++ packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTextWindow/HelpTexts.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./HelpTopicsTextWindow.module.scss";
 
+type AdditionalInfoPropTypes = {
+  additionalInfoVisible: boolean;
+};
+
+const AdditionalInfo: React.VFC<AdditionalInfoPropTypes> = ({
+  additionalInfoVisible,
+}) => {
+  return additionalInfoVisible ? (
+    <div>
+      Cards are stacked in descending order, alternating between red cards and
+      black cards. For example, you can play the two of hearts on the three of
+      clubs
+    </div>
+  ) : null;
+};
+
 export const HowToPlay: React.VFC = () => {
-  const showAdditionalInfo = () => console.log("test");
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false);
 
   return (
     <div className={styles.textContainer}>
@@ -14,9 +30,14 @@ export const HowToPlay: React.VFC = () => {
           to move them to the spaces at the top right fo the screen and then
           making any other plays available on the board.
           <br /> You will be building two kinds of stacks:{" "}
-          <span className={styles.anchor} onMouseOver={showAdditionalInfo}>
+          <span
+            className={styles.anchor}
+            onMouseOver={() => setShowAdditionalInfo(true)}
+            onMouseOut={() => setShowAdditionalInfo(false)}
+          >
             row stacks
           </span>{" "}
+          <AdditionalInfo additionalInfoVisible={showAdditionalInfo} />
           and <span className={styles.anchor}>suit stacks.</span>
           <br /> To free up cards that you need to build a suit stackc, you
           build row stacks. <br /> To move a card or a stack of cards from one
