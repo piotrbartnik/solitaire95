77d35f510af5c7f2ba1f117c82e676652b7f4bae
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-01-31 22:05:40 +0100

    chore: add all text to game help window

--- packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTextWindow/HelpTexts.tsx
+++ packages/solitaire95/src/components/smart-components/HelpTopicsWindow/HelpTopicsTextWindow/HelpTexts.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 import styles from "./HelpTopicsTextWindow.module.scss";
 
-type AdditionalInfoPropTypes = {
-  additionalInfoVisible: boolean;
-};
+const rowStacksInfo = (
+  <div className={styles.externalInfo}>
+    Cards are stacked in descending order, alternating between red cards and
+    black cards. For example, you can play the two of hearts on the three of
+    clubs.
+  </div>
+);
 
-const AdditionalInfo: React.VFC<AdditionalInfoPropTypes> = ({
-  additionalInfoVisible,
-}) => {
-  return additionalInfoVisible ? (
-    <div className={styles.externalInfo}>
-      Cards are stacked in descending order, alternating between red cards and
-      black cards. For example, you can play the two of hearts on the three of
-      clubs
-    </div>
-  ) : null;
-};
+const suitStacksInfo = (
+  <div className={styles.externalInfo}>
+    Cards are stacked in the four areas at the top right of the screen in
+    ascending order, beginning with aces. For example, you can play the two of
+    hearts on the ace of hearts.
+  </div>
+);
 
 export const HowToPlay: React.VFC = () => {
-  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false);
+  const [showRowStacksInfo, setRowStacksInfo] = useState(false);
+  const [showSuitStacksInfo, setSuitStacksInfo] = useState(false);
 
   return (
     <div className={styles.textContainer}>
@@ -32,14 +33,21 @@ export const HowToPlay: React.VFC = () => {
           <br /> You will be building two kinds of stacks:{" "}
           <span
             className={styles.anchor}
-            onMouseOver={() => setShowAdditionalInfo(true)}
-            onMouseOut={() => setShowAdditionalInfo(false)}
+            onMouseOver={() => setRowStacksInfo(true)}
+            onMouseOut={() => setRowStacksInfo(false)}
           >
             row stacks
           </span>
-          <AdditionalInfo additionalInfoVisible={showAdditionalInfo} />
-          and <span className={styles.anchor}>suit stacks.</span>
-          <br /> To free up cards that you need to build a suit stackc, you
+          {showRowStacksInfo && rowStacksInfo} and{" "}
+          <span
+            className={styles.anchor}
+            onMouseOver={() => setSuitStacksInfo(true)}
+            onMouseOut={() => setSuitStacksInfo(false)}
+          >
+            suit stacks.
+          </span>
+          {showSuitStacksInfo && suitStacksInfo}
+          <br /> To free up cards that you need to build a suit stacks, you
           build row stacks. <br /> To move a card or a stack of cards from one
           row to another, drag it. <br />
           To move a card to a suit stack, double-click it.
