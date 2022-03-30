6f27257cebf4b87b06a80aa93849223189bcabc2
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-03-30 21:13:38 +0200

    chore: do not preserve state in local storage for electron app

--- applications/electron-react-app/src/index.tsx
+++ applications/electron-react-app/src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Solitaire95 aboutChildren={<Bio />} />
+    <Solitaire95 aboutChildren={<Bio />} preserveStateInLocalStorage={false} />
   </React.StrictMode>,
   document.getElementById("root")
 );
